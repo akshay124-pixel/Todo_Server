06fc5aaf0209780762e0e9356bb6fe7e11e72258
Akshay Pratap Singh <akshay124-pixel>
2026-02-16 18:00:16 +0530

    fix: update server port to 7000 and change CORS origin to production URL

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import dotenv from "dotenv";
 import router from "./Routes/Route.js";
 import errorhandler from "./Middleware/errorhandler.js";
 const app = express();
-const port = 4000;
+const port = 7000;
 dotenv.config();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://todo-client-bay.vercel.app/",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   }),
