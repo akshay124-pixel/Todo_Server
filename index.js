d1e179dfb0d6335a6a6d1a3a2b12bf3bde2e9669
Akshay Pratap Singh <akshay124-pixel>
2026-02-17 16:18:01 +0530

    Firs Cors issue

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 7000;
 dotenv.config();
 app.use(
   cors({
-    origin: "https://todo-client-bay.vercel.app/",
+    origin: "https://todo-client-bay.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   }),
